84b22d6b1d3fd9c4fabffc3655e19dc23dbd2eac
AleVallejoGarcia <usr2075@salesianos-lacuesta.net>
2023-05-04 16:56:24 +0100

    feat:(App) Creo el menú de el proyecto

--- src/App.java
+++ src/App.java
@@ -1,5 +1,54 @@
+import net.salesianos.entidades.*;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        boolean salir = false;
+       boolean cajaAbierta = false;
+       Scanner teclado = new Scanner(System.in);
+
+       Cajero cajeroHiperDino = new Cajero(1);
+
+       while (salir != true) {
+           System.out.println("a. Abrir caja");
+           System.out.println("b. Añadir un nuevo cliente");
+           System.out.println("c. Atender a un cliente");
+           System.out.println("d. Ver clientes pendientes");
+           System.out.println("e. Cerrar supermecado");
+
+           String opcion = teclado.nextLine();
+           switch (opcion) {
+               case "a":
+                   if (cajaAbierta == false) {
+                       cajaAbierta = true;
+                       System.out.println("Se ha abierto la caja número" + cajeroHiperDino.getNumeroDeCaja());
+                   } else {
+                       System.out.println("La caja ya se encuentra abierta");
+                   }
+                   break;
+               case "b":
+                   if (cajaAbierta == true) {
+                       cajeroHiperDino.anyadirClienteALaCola();
+                   } else {
+                       System.out.println("La caja esta cerrada");
+                   }
+                   break;
+               case "c":
+                   cajeroHiperDino.atenderCliente();
+                   break;
+               case "d":
+                   System.out.println(cajeroHiperDino);
+                   break;
+               case "e":
+                   if (cajaAbierta == true) {
+                       cajaAbierta = false;
+                       System.out.println("Cerranco caja");
+                       salir = true;
+                   } else {
+                       salir = true;
+                   }
+                   break;
+           }
+       }
     }
 }
